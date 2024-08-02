708ca8d8ced4a5bb9e9085f9cb43eb5a3a0d502a
Eduarda Souza <eduarda50438216@aluno.ms.senac.br>
2024-08-01 21:05:13 -0400

    Adicionando arquivo para views

---
+++ Views.py
@@ -0,0 +1,56 @@
+def menu_principal():
+    print("""
+     ONDE DESEJA IR HOJE?  
+===============================
+          1 - Humano
+          2 - Cachorro
+          0 - Sair
+===============================
+          """)
+
+
+def menu_cachorro():
+    print("""
+      O QUE QUER FAZER?
+===============================
+        1 - Criar Cachorro
+        2 - Listar Cachorros
+
+===============================
+          """)
+        # 3 - Interações
+        # 0 - Voltar
+    
+def menu_humano():
+    print("""
+       O QUE QUER FAZER?
+===============================
+          1 - Criar Humano
+          2 - Listar Humanos
+          
+===============================
+          """)
+    # 3 - Interações
+    #       0 - Voltar
+    
+def interacoes_cachorro():
+ print("""
+ O QUE VAI PEDIR PARA SEU DOG?
+===============================
+          1 - 
+          2 - 
+          3 - 
+          0 - Voltar
+===============================
+          """)
+
+def interacoes_humano():
+    print("""
+ O QUE VAI PEDIR PARA SEU HUMANO?
+================================
+          1 - 
+          2 - 
+          3 - 
+          0 - Voltar
+================================
+          """)
